a07ae3e685f5fd13ef0ed1a2af0dd62c79d37b54
kaverinikam007 <kaverinikam007@gmail.com>
2025-09-05 18:30:38 +0530

    Added variable program

---
+++ JavaProgramming/src/Session2/VariableDemo.java
@@ -0,0 +1,49 @@
+package Session2;
+
+public class VariableDemo
+{
+
+	public static void main(String[] args)
+	{
+	
+		//int a;
+		//a=100;
+		
+		/*
+		int a=100;
+		System.out.println(a);
+		
+		a=200;
+		System.out.println(a);
+		*/
+		//Approach 1=If all the variables belongs to different data  types
+		
+		/*
+		   int a=100;
+		   int b=200;
+		   int c=300;
+		   
+		 */
+		
+		//Approach 2=If all the variables belongs to same data  types
+		
+				/*
+				   int a,b,c;
+				       a=100;
+				       b=200;
+				       c=300;
+			    */
+		
+		//Approach 2=If all the variables belongs to same data  types
+		
+		      int a=100, b=200, c = 300;
+		      System.out.println("The value of a is:"+a);
+		      System.out.println("The value of b is:"+b);
+		      System.out.println("The value of c is:"+c);
+		
+		System.out.println(a+"  "+b+"  "+c);
+		
+		
+	}
+
+}
