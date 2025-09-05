a9ef455fc272ef1ea802278bd1b30955476a85cd
kaverinikam007 <kaverinikam007@gmail.com>
2025-09-05 18:27:26 +0530

    Datatype program added

---
+++ JavaProgramming/src/Session2/DataTypaDemo.java
@@ -0,0 +1,44 @@
+package Session2;
+
+public class DataTypaDemo {
+
+	public static void main(String[] args) {
+		//Numeric Data Types
+		
+		int a=100, b=200,  c=300;
+		System.out.println("the value of a is " +a);
+		System.out.println("the value of a is " +b);
+		System.out.println("the value of a is " +c);
+		System.out.println("the sum of a ,b,c is " +(a+b+c));
+		
+		byte by=125;
+		System.out.println(by);
+		
+		short sh=3535;
+		System.out.println(sh);
+		
+		long l= 212356794556l;
+		System.out.println(l);
+		
+		//Decimal Number
+		
+		float item_price=15.5f;
+		System.out.println(item_price);
+		
+		double dl=154.12547896f;
+		System.out.println(dl);
+		
+		char grade='A';
+		System.out.println(grade);
+		
+		String name="Kaveri";
+		System.out.println(name);
+		
+		boolean b1=false;   //boolean allows only true or false
+		System.out.println(b1);
+		
+		
+
+	}
+
+}
